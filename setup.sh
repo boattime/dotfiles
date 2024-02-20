720abd18de5f637ecfa76441658c7775f73720ab
Max Teibel <mteibel@amazon.com>
2024-02-20 10:27:27 -0500

    Add bash script for setting up symlinks

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setup symlinks for configuration files
+ln -s "./nvim" ~/.config/nvim/
+ln -s "./.alacritty.yml" ~/.alacritty.yml
+ln -s "./.tmux.conf" ~/.tmux.conf
+ln -s "./.skhdrc" ~/.skhdrc
+ln -s "./.yabairc" ~/.yabairc
