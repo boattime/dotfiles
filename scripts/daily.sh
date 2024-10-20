77a87f0a94e02f42879c32d3ab6c6b450bcd123e
Max Teibel <mteibel@amazon.com>
2024-10-20 09:02:25 -0400

    Update obsidian scripts

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Get the current date
-current_date=$(date +"%m-%d-%Y")
+current_date=$(date +"%Y-%m-%d")
 current_time=$(date +"%H:%M:%S")
 year=$(date +"%Y")
 week_number=$(date +"%V")
 # Define file paths
-template_file="/Users/max/Documents/notes/templates/daily.md"
-new_file="/Users/max/Documents/notes/daily/$year/W$week_number/$current_date.md"
+template_file="/Users/mteibel/Documents/notes/Templates/Daily.md"
+new_file="/Users/mteibel/Documents/notes/Daily/$year/W$week_number/$current_date.md"
 # Check if the file already exists
 if [ -f "$new_file" ]; then
 	echo "Error: File already exists. Not writing to the file."
 	exit 1
 fi
 # Create the folder structure if it doesn't exist
-mkdir -p /Users/max/Documents/notes/daily/$year/W$week_number
+mkdir -p /Users/mteibel/Documents/notes/Daily/$year/W$week_number
 # Read the template file and replace placeholders with actual values
-sed -e "s/{{title}}/$current_date/g" -e "s/{{date}}/$current_date/g" -e "s/{{time}}/$current_time/g" $template_file >$new_file
+sed -e "s/{{title}}/$current_date/g" \
+  -e "s/{{date}}/$current_date/g" \
+  -e "s/{{time}}/$current_time/g" $template_file >$new_file
 echo "Daily note created: $new_file"
---
+++ scripts/sync.sh
@@ -0,0 +1,5 @@
+cd /Users/mteibel/Documents/notes/ || exit
+git pull
+git add .
+git commit -m "Automated commit at $(date)"
+git push
